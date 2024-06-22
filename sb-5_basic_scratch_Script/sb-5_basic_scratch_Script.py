d6665b0cad89fc97b28d266a20968c68e9487d66
KarthiK-9989 <77263841+KarthiK-9989@users.noreply.github.com>
2024-06-22 21:40:44 +0530

    Synchronizing job sb-5_basic_scratch_Script from AWS Glue at 2024/06/22 16:10:44

---
+++ sb-5_basic_scratch_Script/sb-5_basic_scratch_Script.py
@@ -0,0 +1,21 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql.functions import *
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+df=spark.read.format("csv").load("s3://mytestersbucket/info.csv")
+df1=df.select(col("id"),col("Name"),when(col("Age")>55,"pensionable").otherwise("non-pensionable"))
+df1.write.format("csv").save("s3://mytestersbucket/june23/")
+job.commit()
